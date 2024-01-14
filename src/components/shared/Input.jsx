ed3b59aff4b64a5fe25f8a7eb9a807cefefac2f1
Kocayamak <mikailkocayamak@gmail.com>
2024-01-14 18:21:42 +0300

    register completed

--- src/components/shared/Input.jsx
+++ src/components/shared/Input.jsx
@@ -1,15 +1,39 @@
-import { Text, View, TextInput } from "react-native";
-import React from "react";
-import { Ionicons } from "@expo/vector-icons";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 
-export default function Input({title, icon}) {
-  return (
+export default function Input({
+  title,
+  icon,
+  isSecure = false,
+  secureIcon = false,
+}) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleChangeIsVisible = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return isSecure ? (
     <View className="w-full h-12 rounded-[6px] border-[1px] border-border bg-white relative">
-      <View className="absolute left-[10px] top-[12px]">
-        {icon}
-      </View>
+      <View className="absolute left-[10px] top-[12px]">{icon}</View>
+      <TextInput className="h-full px-[37px] text-[13px]" placeholder={title} secureTextEntry={!isVisible} />
+      <Text className="text-[11px] text-primary absolute top-[-8px] left-[10px] bg-white px-[10px] border-l-[1px] border-r-[1px] border-border">
+        {title}
+      </Text>
+      <TouchableOpacity
+        className="absolute right-[10px] top-[12px]"
+        onPress={handleChangeIsVisible}
+      >
+        {isVisible ? secureIcon.invisible : secureIcon.visible}
+      </TouchableOpacity>
+    </View>
+  ) : (
+    <View className="w-full h-12 rounded-[6px] border-[1px] border-border bg-white relative">
+      <View className="absolute left-[10px] top-[12px]">{icon}</View>
       <TextInput className="h-full px-[37px] text-[13px]" placeholder={title} />
-      <Text className="text-[11px] text-primary absolute top-[-8px] left-[10px] bg-white px-[10px] border-l-[1px] border-r-[1px] border-border">{title}</Text>
+      <Text className="text-[11px] text-primary absolute top-[-8px] left-[10px] bg-white px-[10px] border-l-[1px] border-r-[1px] border-border">
+        {title}
+      </Text>
     </View>
   );
 }
--- src/utils/const/authForm.js
+++ src/utils/const/authForm.js
@@ -2,41 +2,56 @@ import { Ionicons, Feather } from "@expo/vector-icons";
 
 export const loginForm = [
   {
-    id : 1,
+    id: 1,
     title: "E-Mail Adresi",
     icon: <Ionicons name="mail" size={20} color="#868E96" />,
   },
   {
-    id : 2,
+    id: 2,
     title: "Şifre",
     icon: <Ionicons name="key-outline" size={20} color="#868E96" />,
+    isSecure: true,
+    secureIcon: {
+      visible: <Feather name="eye" size={20} color="#868E96" />,
+      invisible: <Feather name="eye-off" size={20} color="#868E96" />,
+    },
   },
 ];
 
 export const registerForm = [
-    {
-        id : 1,
-        title : "Ad",
-        icon : <Feather name="user" size={20} color="#868E96" />
+  {
+    id: 1,
+    title: "Ad",
+    icon: <Feather name="user" size={20} color="#868E96" />,
+  },
+  {
+    id: 2,
+    title: "Soyad",
+    icon: <Feather name="user" size={20} color="#868E96" />,
+  },
+  {
+    id: 3,
+    title: "E-Mail Adresi",
+    icon: <Ionicons name="mail" size={20} color="#868E96" />,
+  },
+  {
+    id: 4,
+    title: "Şifre",
+    icon: <Ionicons name="key-outline" size={20} color="#868E96" />,
+    isSecure: true,
+    secureIcon: {
+      visible: <Feather name="eye" size={20} color="#868E96" />,
+      invisible: <Feather name="eye-off" size={20} color="#868E96" />,
     },
-    {
-        id : 2,
-        title : "Soyad",
-        icon : <Feather name="user" size={20} color="#868E96" />
+  },
+  {
+    id: 5,
+    title: "Şifre Tekrar",
+    icon: <Ionicons name="key-outline" size={20} color="#868E96" />,
+    isSecure: true,
+    secureIcon: {
+      visible: <Feather name="eye" size={20} color="#868E96" />,
+      invisible: <Feather name="eye-off" size={20} color="#868E96" />,
     },
-    {
-        id : 3,
-        title : "E-Mail Adresi",
-        icon : <Ionicons name="mail" size={20} color="#868E96" />
-    },
-    {
-        id : 4,
-        title : "Şifre",
-        icon : <Ionicons name="key-outline" size={20} color="#868E96" />
-    },
-    {
-        id : 5,
-        title : "Şifre Tekrar",
-        icon : <Ionicons name="key-outline" size={20} color="#868E96" />
-    }
-]
+  },
+];
